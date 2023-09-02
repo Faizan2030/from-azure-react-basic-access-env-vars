d8f1ffc38381138faa070ff9e334635b81adc48b
Faizan <faizan.rauf@ercot.com>
2023-09-02 15:57:01 -0500

    process.env.THE_APP_URL

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  return <div>Hello {process.env.THE_APP_URL}</div>;
 }
 
 export default App;
